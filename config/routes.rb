421ffeb9579a4b86a875cd7bf0a108bbd018f7dd
rios1993 <rios1993@outlook.com>
2015-06-04 00:35:32 +0000

    se agrego bd formato mysql

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :orders
+  resources :dishes
+  resources :categories
+  resources :menus
+  resources :restaurants
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
